cc44c9a44fa1e4155619a9f5bf063d303f4032ad
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-08 12:07:02 +0800

    feat: priority_queue

--- lib/list/list.go
+++ lib/list/list.go
@@ -1,23 +1,7 @@
 package list
 
-type node[T any] struct {
-	val  T
-	prev *node[T]
-	next *node[T]
-}
+import "cmp"
 
-type LinkedList[T any] struct {
-	head *node[T]
-	tail *node[T]
-	size int
-}
+func Sum[T cmp.Ordered]() {
 
-func NewLinkedList[T any]() *LinkedList[T] {
-	head, tail := &node[T]{}, &node[T]{}
-	head.next = tail
-	tail.prev = head
-	return &LinkedList[T]{
-		head: head,
-		tail: tail,
-	}
 }
--- webook-server/internal/web/user.go
+++ webook-server/internal/web/user.go
@@ -1,6 +1,9 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type UserHandler struct {
 }
@@ -9,13 +12,42 @@ const (
 // todo regex
 )
 
+type Resp struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
 func (u *UserHandler) Profile(ctx *gin.Context) {
 
 }
 
 func (u *UserHandler) Login(ctx *gin.Context) {
-
+	type Req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	var req Req
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		return
+	}
+	ctx.JSON(http.StatusOK, Resp{
+		Code: 0,
+		Msg:  "登录成功",
+	})
 }
 func (u *UserHandler) Register(ctx *gin.Context) {
-
+	type Req struct {
+		UserName        string `json:"user_name"`
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirm_password"`
+	}
+	var req Req
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		return
+	}
+	ctx.JSON(http.StatusOK, Resp{
+		Code: 0,
+		Msg:  "注册成功",
+	})
 }
