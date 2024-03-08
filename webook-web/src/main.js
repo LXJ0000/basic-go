f76f9b0c37faf292d4252fe3b36f78a1ee249597
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-08 18:32:52 +0800

    feat: webook-web building ...

--- webook-web/src/main.js
+++ webook-web/src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+async function bootstrap() {
+    const app = createApp(App)
+    setupStore(app) // 优先级最高
+    await setupRouter(app)
+    app.mount('#app')
+}
+bootstrap()
