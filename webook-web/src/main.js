ee12bb14991da22b2489da530894625c8d8f8a11
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-13 09:29:02 +0800

    feat: 创建空vue项目 安装pinia

--- webook-web/src/main.js
+++ webook-web/src/main.js
@@ -1,18 +1,6 @@
+import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
 
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-const app = createApp(App)
-const pinia = createPinia()
-
-pinia.use(piniaPluginPersistedstate)
-app.use(pinia)
-app.use(router)
-app.mount('#app')
-
-
+createApp(App).mount('#app')
