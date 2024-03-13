488f0b066d04ecd111b3cdb06256c3d3b2929c87
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-13 17:22:36 +0800

    feat: ioc

---
+++ webook-server/ioc/db.go
@@ -0,0 +1,19 @@
+package ioc
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"webook-server/config"
+	"webook-server/internal/repository/dao"
+)
+
+func InitDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+	if err != nil {
+		panic(err)
+	}
+	if err = dao.InitTable(db); err != nil {
+		panic(err)
+	}
+	return db
+}
