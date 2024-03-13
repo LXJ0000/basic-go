704c92b3f13f63e1ca11f1c4b664778fa4793653
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-13 21:46:19 +0800

    feat: slice Map

--- lib/slice/slice.go
+++ lib/slice/slice.go
@@ -92,3 +92,11 @@ func Unique[T lib.Ordered](nums []T) []T {
 	Shrink(newNums)
 	return newNums
 }
+
+func Map[Src any, Dst any](src []Src, m func(idx int, src Src) Dst) []Dst {
+	dst := make([]Dst, len(src))
+	for i, s := range src {
+		dst[i] = m(i, s)
+	}
+	return dst
+}
