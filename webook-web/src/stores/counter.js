4a1a458892d1612e39096639a89ea9bca46dc812
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-13 09:47:38 +0800

    feat: counter

--- webook-web/src/stores/counter.js
+++ webook-web/src/stores/counter.js
@@ -1,12 +1,14 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { computed } from "@vue/reactivity";
+import { defineStore } from "pinia";
+import { ref } from 'vue'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
-  function increment() {
+
+  const increment = () => {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  return { count, increment }
 })
