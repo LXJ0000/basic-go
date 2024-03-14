ec8e913637377afbb25e1e59e3701212468ca326
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-14 09:55:14 +0800

    fix: split project

--- lib/errs/errors.go
+++
@@ -1,9 +0,0 @@
-package errs
-
-import "fmt"
-
-const pkgName = "lib"
-
-func NewErrIndexOutOfRange(length int, index int) error {
-	return fmt.Errorf("[%s] index out of range [%d] with length %d", pkgName, length, index)
-}
