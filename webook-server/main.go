84fd2a24fd6ab8b2b446adb4f157746c6f903fb1
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-25 20:40:22 +0800

    commit

--- webook-server/main.go
+++ webook-server/main.go
@@ -15,7 +15,6 @@ const (
 func main() {
 	snowflake.Init("2023-01-01", 1)
 	r := InitWebServer(window, rate)
-	////initViperV2()
 	_ = r.Run(":8080")
 }
 
@@ -62,4 +61,57 @@ func main() {
 //			panic(fmt.Errorf("unmarshal conf failed, err:%s \n", err))
 //		}
 //	})
+//}
+//func initViper() {
+//	viper.SetConfigName("dev")
+//	viper.SetConfigType("yaml")
+//	viper.AddConfigPath("./config")
+//	if err := viper.ReadInConfig(); err != nil {
+//		panic(err)
+//	} // 加载到内存
+//}
+//
+//func initViperV1() {
+//	cFile := pflag.String("config", "./dev.yaml", "指定配置文件路径")
+//	pflag.Parse()
+//	viper.SetConfigFile(*cFile)
+//}
+//func initViperV2() {
+//	viper.SetConfigType("yaml")
+//	if err := viper.AddRemoteProvider("etcd3", "127.0.0.1:23790", "/webook"); err != nil {
+//		panic(err)
+//	}
+//	if err := viper.ReadRemoteConfig(); err != nil {
+//		panic(err)
+//	}
+//}
+//
+//func initViperV3() {
+//	viper.SetConfigFile(confFile) // 指定配置文件路径
+//	// 读取配置信息
+//	if err := viper.ReadInConfig(); err != nil { // 读取配置信息失败
+//		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+//	}
+//	// 将读取的配置信息保存至全局变量Conf
+//	if err := viper.Unmarshal(Conf); err != nil {
+//		panic(fmt.Errorf("unmarshal conf failed, err:%s \n", err))
+//	}
+//	// 监控配置文件变化
+//	viper.WatchConfig()
+//	// 注意！！！配置文件发生变化后要同步到全局变量Conf
+//	viper.OnConfigChange(func(in fsnotify.Event) {
+//		fmt.Println("夭寿啦~配置文件被人修改啦...")
+//		if err := viper.Unmarshal(Conf); err != nil {
+//			panic(fmt.Errorf("unmarshal conf failed, err:%s \n", err))
+//		}
+//	})
+//}
+//
+//func initLogger() {
+//	logger, err := zap.NewDevelopment()
+//	if err != nil {
+//		panic(err)
+//	}
+//	zap.ReplaceGlobals(logger)
+//	zap.L().Info("ok")
 //}
